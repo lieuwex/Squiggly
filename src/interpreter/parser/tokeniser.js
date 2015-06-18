6ca56476bec1efbed571ce0388b2b9d4c186dd17
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2015-06-18 18:15:01 +0200

    new tokeniser

--- src/interpreter/parser/tokeniser.js
+++ src/interpreter/parser/tokeniser.js
@@ -27,7 +27,7 @@ const DEBUG = false;
 
 	// TODO:
 	//	 ~ Ignore tokens while in string.
-	//	 ~ Have an ignore token for whitspace. (this will be ignored in strings,
+	//	 ~ Have an ignore token for whitespace. (this will be ignored in strings,
 	//	   see #1) (or at least handle whitespace).
 	//	 ~ Default type for normal words.
 	//	 ~ Don't ignore escaped strings, instead give them their own token and
@@ -77,38 +77,7 @@ const DEBUG = false;
 	}
 
 	function Tokeniser () {
-		var _currentWalkPossibleTokens = tokensMap;
-		var _currentWalkedOn = [];
-		var _currentWalkIndex = 0;
-		var _currentWalkStart = 0;
-		var _currentWalkUnknown = false;
-
-		var _currentStringInfo = {
-			// The string token that started this string. If null we're not in a string.
-			token: null,
-			content: "",
-			start: null,
-			end: null,
-
-			reset: function () {
-				this.token = null;
-				this.content = "";
-				this.start = null;
-				this.end = null;
-			}
-		}
-
-		var resetCurrentWalk = function () {
-			_currentWalkPossibleTokens = tokensMap;
-			_currentWalkedOn = [];
-			_currentWalkIndex = 0;
-			_currentWalkStart = 0;
-			_currentWalkUnknown = false;
-		}
-
-		var inString = function () {
-			return _currentStringInfo.token != null;
-		}
+		var _currentWord = '';
 
 		var error = function (errType, lineNumber, charIndex) {
 			var str = "";
@@ -140,205 +109,131 @@ const DEBUG = false;
 			};
 
 			throw new Error(str);
-		}
+		};
 
 		return {
 			tokens: [],
 
 			walkOverLine: function (str, lineNumber) {
-				var _tokens = this.tokens;
+				var _tokens = [];
 
-				// This will be hint when an unknown symbol or EOL is hit.
-				var tryFinishCurrentWalk = function (char) {
-					if (inString()) {
-						error(
-							"string_not_ended",
-							lineNumber,
-							_currentStringInfo.start
-						);
-					} else if (_currentWalkIndex === 0) {
-						// We aren't in a walk.
-						if (_currentWalkUnknown) {
-							_tokens.push({
-								token: "word",
-								content: _currentWalkedOn.join(""),
-								start: char - _currentWalkedOn.length,
-								end: char
-							})
-						} else {
-							return false;
+				for (var currentChar = 0; currentChar < str.length; currentChar++) {
+					var char = str[currentChar];
+
+					var tokens = _(tokensMap)
+						.pairs()
+						.filter(function (pair) {
+							var x = true;
+							if (/\w/.test(pair[0][0])) { // for keyword type statments we want a non word char before them.
+								x = currentChar === 0 ||
+									/\W/.test(str[currentChar - 1])
+							}
+							return x && pair[0][0] === char;
+						})
+						.sortBy(function (pair) {
+							return pair[0].length;
+						})
+						.reverse()
+						.value();
+
+					var anyToken = false;
+					_.forEach(tokens, function (token) {
+						for (var i = 1; i < token[0].length; i++) {
+							if (str[currentChar + i] !== token[0][i]) {
+								// can't be this token.
+								return;
+							}
 						}
-					} else {
-						var tokens = findTokens(function (token, tokenType) {
-							return _currentWalkedOn.join("") === token;
-						});
 
-						if (tokens.length === 1) {
-							_tokens.push({
-								token: tokens[0][1],
-								start: _currentWalkStart,
-								end: _currentWalkStart + _currentWalkIndex - 1
-							});
-							return true;
+						if (token[1] === 'interpolate_string') {
+							error(
+								'not_in_string',
+								lineNumber,
+								currentChar
+							);
 						}
-						return false;
-					}
-				}
 
-				function tokenStartsWith (str) {
-					return _(tokensMap).keys().any(function (token) {
-						return token.indexOf(str) === 0;
-					});
-				}
+						anyToken = true;
 
-				for (var charPos = 0; charPos < str.length; charPos++) {
-					var char = str[charPos];
-					var token = null;
-
-					var tokens = findTokens(function (token, tokenType) {
-						return char === token[_currentWalkIndex];
-					}, _currentWalkPossibleTokens);
-
-					if (
-						tokens.length === 1 &&
-						(_currentWalkUnknown ||
-						tokens[0][0] === _currentWalkedOn.concat([ char ]).join(""))
-					) {
-						token = tokens[0];
-					}
-
-					if (DEBUG) {
-						console.log(
-							charPos,
-							char,
-							_currentWalkIndex,
-							tokens,
-							(token && token[1]),
-							_currentWalkedOn
-						);
-					}
-
-					if (token != null) { // Token found
-						if (token[1] === "interpolate_string") {
-							if (_.contains(interpolationStrings, _currentStringInfo.token)) {
-								var interpolateStart     = charPos + 1,
-								    interpolateEnd       = null,
-									interpolationContent = "";
-
-								for (var i = interpolateStart; i < str.length; i++) {
-									var char = str[i];
-									if (tokensMap[char] === "interpolate_string") {
-										interpolateEnd = i;
-										break;
-									} else {
-										interpolationContent += char;
-									}
-								}
-								if (interpolateEnd == null) {
+						if (token[1] === 'string') {
+							var i = currentChar + 1;
+							var reachedStringEnd = false;
+							while (!reachedStringEnd) {
+								var c = str[i];
+								if (c === token[0] && str[i-1] != '\\') {
+									reachedStringEnd = true;
+								} else if (c == null) {
 									error(
-										"interpolation_not_ended",
+										'string_not_ended',
 										lineNumber,
-										charPos
+										currentChar
 									);
 								}
+								i++;
+							}
+							var begin = currentChar;
+							currentChar = i;
+							var t = {
+								token: 'string',
+								content: str.substring(begin + 1, currentChar - 1),
+								begin: begin,
+								end: currentChar
+							};
+							var regResult = /\\?`.*\\?`/.exec(t.content);
+
+							if (
+								regResult !== null &&
+								regResult[0] !== '\\' &&
+								regResult[regResult.length - 2] !== '\\'
+							) {
+								var interpolation = regResult[0];
+								var content = interpolation.substring(1, interpolation.length - 1);
+								var interpolateStart = str.indexOf(interpolation) + 1;
+
+								t.content = t.content.replace(interpolation, "");
 
 								var tokeniser = new Tokeniser();
-								tokeniser.walkOverLine(interpolationContent);
+								tokeniser.walkOverLine(content);
 								tokeniser.tokens.forEach(function (token) {
 									_tokens.push(_.extend(token, {
 										// The position of the new tokeniser resets at index 0.
 										// This is not inline with the string we're in, adding
 										// the starting position of the interpolation to the
 										// start and end of every token should fix it.
-										start: token.start + interpolateStart,
+										begin: token.begin + interpolateStart,
 										end: token.end + interpolateStart
 									}));
 								});
-								charPos = interpolateEnd; // skip over interpolation
-							} else if (inString()) {
-								error("not_in_interpolation_string", lineNumber, charPos);
-							} else {
-								error("not_in_string", lineNumber, charPos);
-							}
-						} else if (token[1] === "string" && !_currentStringInfo.inInterpolation) {
-							if (_currentStringInfo.token === char) { // We were in a string and token is the same as the current string's token -> string ended.
-								_currentStringInfo.token = null;
-								_currentStringInfo.end;
-								this.tokens.push({
-									token: "string",
-									content: _currentStringInfo.content,
-									start: _currentStringInfo.start,
-									end: charPos
-								});
-							} else { // We weren't in a string, token is string -> string started.
-								_currentStringInfo.token = char;
-								_currentStringInfo.start = charPos;
-							}
-						} else if (!inString()) { // Non-string token.
-							this.tokens.push({
-								token: token[1],
-								start: charPos - _currentWalkIndex,
-								end: charPos
-							});
-						}
-
-						resetCurrentWalk();
-						continue;
-					} else if (inString()) { // Add string content
-						if (char === '\\') {
-							var nextChar = str[charPos + 1];
-
-							if (nextChar === '\\') {
-								_currentStringInfo.content += '\\';
-							} else if (nextChar === 'n') {
-								_currentStringInfo.content += '\n';
-							} else if (nextChar === 't') {
-								_currentStringInfo.content += '\t';
-							} else if (nextChar === '"') {
-								_currentStringInfo.content += '"';
-							} else if (nextChar === "'") {
-								_currentStringInfo.content += "'";
-							} else {
-								continue;
 							}
 
-							charPos++;
+							_tokens.push(t);
 						} else {
-							_currentStringInfo.content += char;
+							var begin = currentChar;
+							currentChar = currentChar + token[0].length - 1;
+							_tokens.push({
+								token: token[1],
+								begin: begin,
+								end: currentChar
+							});
+							return false; // break
 						}
-						continue;
-					} else if (tokens.length === 0 && !_currentWalkUnknown) { // No token.
-						// Try to finish previous walk.
-						var walkEnded = tryFinishCurrentWalk(charPos);
-						if (walkEnded) {
-							resetCurrentWalk();
-							continue;
-						}
-					}
+					});
 
-					if (_currentWalkIndex === 0) {
-						_currentWalkStart = charPos;
-					}
-					if (tokenStartsWith(char)) {
-						if (_currentWalkUnknown) {
-							tryFinishCurrentWalk(charPos);
-							resetCurrentWalk();
-						}
+					if (!anyToken && !/\s/.test(char)) {
+						_currentWord += char;
+					} else if (_currentWord.length > 0) {
+						_tokens.push({
+							token: 'word',
+							content: _currentWord,
+							begin: currentChar - _currentWord.length,
+							end: currentChar
+						});
 
-						_currentWalkIndex++;
-						_currentWalkPossibleTokens = _.zipObject(tokens);
-						_currentWalkedOn.push(char);
-					} else if (char.trim() === char) {
-						_currentWalkedOn.push(char);
-						_currentWalkUnknown = true;
+						_currentWord = '';
 					}
 				}
-
-				tryFinishCurrentWalk(str.length - 1);
-				_currentStringInfo.reset();
-				_.remove(this.tokens, function (token) {
-					return _.isEmpty(token.token);
-				});
+				this.tokens = this.tokens.concat(_tokens);
+				this.tokens = _.sortBy(this.tokens, "begin");
 			}
 		}
 	}
